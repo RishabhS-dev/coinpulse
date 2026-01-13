f91184b65b1e0a569a356c031f79520074ac64d7
RishabhS-dev <rishabhsinghprofessional@gmail.com>
2026-01-13 19:46:39 +0530

    preproject raw files need fix

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,41 +1,64 @@
-import{
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableCaption,
-} from "@/components/ui/table";
-import { cn } from "@/lib/utils";
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { cn } from '@/lib/utils';
 
-const DataTable = <T,>({columns, data, rowKey, tableClassName, headerRowClassName,headerClassName, headerCellClassName, bodyRowClassName, bodyCellClassName}: DataTableProps<T>) => {
+const DataTable = <T,>({
+  columns,
+  data,
+  rowKey,
+  tableClassName,
+  headerClassName,
+  headerRowClassName,
+  headerCellClassName,
+  bodyRowClassName,
+  bodyCellClassName,
+}: DataTableProps<T>) => {
   return (
     <Table className={cn('custom-scrollbar', tableClassName)}>
-  <TableCaption>A list of your recent invoices.</TableCaption>
-  <TableHeader className={headerClassName}>
-    <TableRow className={cn('hover:bg-transparent!', headerRowClassName)}>
-      {columns.map((column,i) => (
-        <TableHead key={i} className={cn('bg-dark-400 text-purple-100 py-4 first:pls-5 last:pr-5')}>
-          {column.header}
-        </TableHead>
-      ))}
-    </TableRow>
-  </TableHeader>
-  <TableBody>
-    {data.map((row, rowIndex) => (
-      <TableRow key={rowKey(row, rowIndex)} className={cn('overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative', bodyRowClassName)}>
-        {columns.map((column, columnIndex) => (
-          <TableCell key={columnIndex} className={cn('py-4 first:pl-5 last:pr-5', bodyCellClassName)}>
-            {column.cell(row, rowIndex)}
-          </TableCell>
+      <TableHeader className={headerClassName}>
+        <TableRow className={cn('hover:bg-transparent!', headerRowClassName)}>
+          {columns.map((column, i) => (
+            <TableHead
+              key={i}
+              className={cn(
+                'bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5',
+                headerCellClassName,
+                column.headClassName,
+              )}
+            >
+              {column.header}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((row, rowIndex) => (
+          <TableRow
+            key={rowKey(row, rowIndex)}
+            className={cn(
+              'overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative',
+              bodyRowClassName,
+            )}
+          >
+            {columns.map((column, columnIndex) => (
+              <TableCell
+                key={columnIndex}
+                className={cn('py-4 first:pl-5 last:pr-5', bodyCellClassName, column.cellClassName)}
+              >
+                {column.cell(row, rowIndex)}
+              </TableCell>
+            ))}
+          </TableRow>
         ))}
-      </TableRow>
-    ))}
-  </TableBody>
-</Table>
-  )
-}
+      </TableBody>
+    </Table>
+  );
+};
 
-export default DataTable
+export default DataTable;
